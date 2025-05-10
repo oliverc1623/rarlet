f1eaf00f45d6ab2aa2bbe1397d2a7371abc51bc3
Oliver Chang <oliverc1622@gmail.com>
2025-05-10 00:11:17 +0000

    dummy test

--- rarlet/tests/plotting_test.py
+++ rarlet/tests/plotting_test.py
@@ -6,3 +6,10 @@ from pathlib import Path
 root_path = Path(__file__).resolve().parent.parent
 
 # %%
+
+
+def test_get_vector_env_seeding() -> None:
+    """Test that each environment in the vectorized environment has a different seed."""
+    x = 1
+    y = 1
+    assert x == y
