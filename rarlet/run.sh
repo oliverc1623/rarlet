e0dedb5eb9f61cf83aa84d4b5de6b2cdc326f519
Oliver Chang <oliverc1622@gmail.com>
2025-05-13 23:16:52 +0000

    run sac protagonist

---
+++ rarlet/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Execute scripts with different seeds and additional arguments for torchcompile scripts
+scripts=(
+    sac_metadrive_protagonist.py
+)
+for script in "${scripts[@]}"; do
+    for seed in 21 31 41; do
+        if [[ $script == *_torchcompile.py ]]; then
+            python $script --seed=$seed --exp_name "SAC_protagonist" --num-envs 8 --gradient_steps -1 --cudagraphs --compile
+        else
+            python $script --seed=$seed
+        fi
+    done
+done
