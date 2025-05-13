bec50d56cafa44af7fd4fc50f86036abeee52069
Oliver Chang <oliverc1622@gmail.com>
2025-05-13 23:21:31 +0000

    1 more seed + catch files

--- rarlet/run.sh
+++ rarlet/run.sh
@@ -4,8 +4,8 @@ scripts=(
     sac_metadrive_protagonist.py
 )
 for script in "${scripts[@]}"; do
-    for seed in 21 31 41; do
-        if [[ $script == *_torchcompile.py ]]; then
+    for seed in 21 31 41 51; do
+        if [[ $script == *_protagonist.py ]]; then
             python $script --seed=$seed --exp_name "SAC_protagonist" --num-envs 8 --gradient_steps -1 --cudagraphs --compile
         else
             python $script --seed=$seed
