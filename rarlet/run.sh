70fe3cd727ee3b68b33e6a9e9aff2199bcfa1a0c
Oliver Chang <oliverc1622@gmail.com>
2025-05-27 09:11:17 +0000

    add traffic densities to script

--- rarlet/run.sh
+++ rarlet/run.sh
@@ -3,10 +3,23 @@
 scripts=(
     sac_metadrive_protagonist.py
 )
+traffic_densities=(0.0 0.2 0.3)
 for script in "${scripts[@]}"; do
     for seed in 31 41 51; do
         if [[ $script == *_protagonist.py ]]; then
-            python $script --seed=$seed --exp_name "lane-follow" --num-envs 4 --gradient_steps -1 --cudagraphs --compile --map "SSS" --num-lanes 2 --no-random_spawn_lane_index --total-timesteps 500_000
+            for density in "${traffic_densities[@]}"; do
+                python $script --seed=$seed \
+                    --exp_name "lane-follow" \
+                    --num-envs 4 \
+                    --gradient_steps -1 \
+                    --cudagraphs \
+                    --compile \
+                    --map "SSS" \
+                    --num-lanes 2 \
+                    --no-random_spawn_lane_index \
+                    --total-timesteps 1000 \
+                    --traffic-density $density
+            done
         else
             python $script --seed=$seed
         fi
