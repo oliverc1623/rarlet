f6915bd52ef5945e89f765bf04b9cea988107ebd
Oliver Chang <oliverc1622@gmail.com>
2025-05-27 07:40:46 +0000

    format bash script

--- rarlet/run.sh
+++ rarlet/run.sh
@@ -4,9 +4,19 @@ scripts=(
     sac_metadrive_protagonist.py
 )
 for script in "${scripts[@]}"; do
-    for seed in 21 31 41 51; do
+    for seed in 31 41 51; do
         if [[ $script == *_protagonist.py ]]; then
-            python $script --seed=$seed --exp_name "SAC_protagonist" --num-envs 8 --gradient_steps -1 --cudagraphs --compile
+            python $script \
+                --seed=$seed \
+                --exp_name "lane-follow" \
+                --num-envs 8 \
+                --gradient_steps -1 \
+                --cudagraphs \
+                --compile \
+                --map "SSS" \
+                --num-lanes 2 \
+                --no-random_spawn_lane_index \
+                --total-timesteps 500_000
         else
             python $script --seed=$seed
         fi
