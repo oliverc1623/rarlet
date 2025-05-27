172f83cc4455bfa6cca6a58b12f8f7906c815c42
Oliver Chang <oliverc1622@gmail.com>
2025-05-27 08:52:19 +0000

    one liner

--- rarlet/run.sh
+++ rarlet/run.sh
@@ -6,17 +6,7 @@ scripts=(
 for script in "${scripts[@]}"; do
     for seed in 31 41 51; do
         if [[ $script == *_protagonist.py ]]; then
-            python $script \
-                --seed=$seed \
-                --exp_name "lane-follow" \
-                --num-envs 8 \
-                --gradient_steps -1 \
-                --cudagraphs \
-                --compile \
-                --map "SSS" \
-                --num-lanes 2 \
-                --no-random_spawn_lane_index \
-                --total-timesteps 500_000
+            python $script --seed=$seed --exp_name "lane-follow" --num-envs 4 --gradient_steps -1 --cudagraphs --compile --map "SSS" --num-lanes 2 --no-random_spawn_lane_index --total-timesteps 500_000
         else
             python $script --seed=$seed
         fi
